09fd6d5cbae0f19cb3160d2b37e28164658497f5
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-07-24 18:02:47 +0100

    create soup object parser

--- PriceTracker.py
+++ PriceTracker.py
@@ -1,4 +1,4 @@
-import bs4  # todo import beautiful soup package
+from bs4 import BeautifulSoup
 import requests
 import smtplib
 
@@ -6,6 +6,10 @@ URL = 'https://www.konga.com/product/apple-iphone-11-128gb-rom-4gb-ram-ios-6-1-w
 
 headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
                          'Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.40'}
+
+page = requests.get(URL, headers=headers)
+
+soup = BeautifulSoup(page.content, 'html.parser')
 
 def pricetracker():  # todo complete price tracker function
     pass
