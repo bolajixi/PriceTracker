c6196f6a5755005cb9de813b0e9f8e378ff592a3
Bolaji Olawale <bolaji.olawale007@gmail.com>
2020-07-24 17:55:37 +0100

    included web browser header

--- PriceTracker.py
+++ PriceTracker.py
@@ -2,6 +2,10 @@ import bs4  # todo import beautiful soup package
 import requests
 import smtplib
 
+URL = 'https://www.konga.com/product/apple-iphone-11-128gb-rom-4gb-ram-ios-6-1-white-4823239'
+
+headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) '
+                         'Chrome/84.0.4147.89 Safari/537.36 Edg/84.0.522.40'}
 
 def pricetracker():  # todo complete price tracker function
     pass
